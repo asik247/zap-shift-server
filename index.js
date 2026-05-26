925a3a9b4ca65018673b501f9fb83cc74c55d22d
Asikur Rahaman <mdasikur5893@gmail.com>
2026-05-26 23:57:27 +0600

    next step code write now

--- index.js
+++ index.js
@@ -140,6 +140,7 @@ async function run() {
 
         //! now create checkout-session for parcles 2;
         //! then success id post this server and retrive and some info post db and send cliend site and show tracking id and tranzation id;
+        //Todo next setp pland this no-repated db data post this info;
        
 
 

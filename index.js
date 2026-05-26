8d4693f526cd3cb01ed879a8eaa670fa9889263f
Asikur Rahaman <mdasikur5893@gmail.com>
2026-05-26 23:55:43 +0600

    next step plan already complete

--- index.js
+++ index.js
@@ -139,6 +139,7 @@ async function run() {
         })
 
         //! now create checkout-session for parcles 2;
+        //! then success id post this server and retrive and some info post db and send cliend site and show tracking id and tranzation id;
        
 
 
